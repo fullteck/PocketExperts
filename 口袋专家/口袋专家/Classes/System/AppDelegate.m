e36c82164a16cff834b468af783b331bcc6b41a8
cls <changls01@163.com>
2015-06-19 11:52:43 +0800

    新的工程

--- 口袋专家/口袋专家/Classes/System/AppDelegate.m
+++ 口袋专家/口袋专家/Classes/System/AppDelegate.m
@@ -2,13 +2,11 @@
 //  AppDelegate.m
 //  口袋专家
 //
-//  Created by 扶摇直上 on 15/6/18.
-//  Copyright © 2015年 扶摇直上. All rights reserved.
+//  Created by 扶摇直上 on 15/6/19.
+//  Copyright (c) 2015年 扶摇直上. All rights reserved.
 //
 
 #import "AppDelegate.h"
-#import "KDBaseNavigationController.h"
-#import "PPRevealSideViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,20 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    //    [QMapServices sharedServices].apiKey = @"";
-    
-//    MainViewController *mainViewController = [[MainViewController alloc] init];
-    KDBaseNavigationController *navigationController = [[KDBaseNavigationController alloc] init];
-    
-    self.window.rootViewController = [[PPRevealSideViewController alloc] initWithRootViewController:navigationController];
-    
-    self.window.backgroundColor = [UIColor whiteColor];
-    
-    [self.window makeKeyAndVisible];
-    
     return YES;
 }
 
